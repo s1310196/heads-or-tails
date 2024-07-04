cb30b9036e91f6831e3153a53d4ef74f05208422
YANAGISAWA Takumi <s1310196@sshsv212.u-aizu.ac.jp>
2024-07-05 02:00:04 +0900

    HoT: initail revision

---
+++ heads_or_tails.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+const char* coin_toss();
+
+int main(){
+    srand(time(NULL));
+
+    printf("Tossing a coin...\n");
+
+    int heads=0,tails=0,i;
+    const char* results[3];
+
+    for(i=0;i<3;i++){
+        results[i]=coin_toss();
+        if(results[i][0]=='H')heads++;
+        else tails++;
+        printf("Round %d: %s\n",i+1,results[i]);
+    }
+
+    printf("Heads: %d:, Tails: %d\n",heads,tails);
+}
+
+const char* coin_toss(){
+    return rand()%2==0?"Heads":"Tails";
+}
